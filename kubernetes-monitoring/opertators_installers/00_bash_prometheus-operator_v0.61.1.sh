701670445536dd8776ff722229855b81c2265a12
Ivan Staroselskiy <i.staroselskiy@gmail.com>
2022-12-29 17:30:50 +0300

    monitoring some state

--- kubernetes-monitoring/opertators_installers/00_bash_prometheus-operator_v0.61.1.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-kubectl create -n prometheus -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/bundle.yaml || \
-kubectl apply -n prometheus -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/bundle.yaml
--- kubernetes-monitoring/opertators_installers/90_bash_prometheus-operator_v0.61.1.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-kubectl delete -n prometheus -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/bundle.yaml
